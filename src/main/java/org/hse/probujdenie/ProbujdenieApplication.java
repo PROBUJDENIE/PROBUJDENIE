2bbc28cc9c27f2309e3c0c7e9db2cb1692a2b09d
Vladimir_Belikov <belikov_v2@mail.ru>
2026-01-24 02:16:03 +0300

    feat: initial commit

---
+++ src/main/java/org/hse/probujdenie/ProbujdenieApplication.java
@@ -0,0 +1,13 @@
+package org.hse.probujdenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbujdenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProbujdenieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hse/probujdenie/ProbujdenieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hse.probujdenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbujdenieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

86f8ab13d1d1e601841309ca26a21e1cd545244c
Vitalia_Prokhorova <username@yandex.ru>
2026-02-10 16:42:23 +0300

    feat: добавить правила проверки для заданий

--- src/main/frontend/src/constructor/components/content/blockPannel/blocks/TaskBlock.jsx
+++ src/main/frontend/src/constructor/components/content/blockPannel/blocks/TaskBlock.jsx
@@ -3,6 +3,8 @@ export default function TaskBlock({ block, onChange }) {
     const title = content.title ?? "";
     const description = content.description ?? "";
     const initialCode = content.initialCode ?? "";
+    const inputCode = content.inputCode ?? "";
+    const outputCode = content.outputCode ?? "";
 
     const handleChange = (field, newValue) => {
         onChange({
@@ -13,25 +15,42 @@ export default function TaskBlock({ block, onChange }) {
 
     return (
         <div className="block block-task">
+            <h1 style={{  fontSize: "18px", fontWeight: "700"}}>Название </h1>
             <input
                 type="text"
                 placeholder="Введите название задания"
                 value={title}
                 onChange={(e) => handleChange("title", e.target.value)}
             />
-
+            <h1 style={{  fontSize: "18px", fontWeight: "700"}}>Описание </h1>
             <textarea
                 placeholder="Введите описание задачи"
                 value={description}
                 onChange={(e) => handleChange("description", e.target.value)}
                 rows={3}
             />
-
+            <h1 style={{ fontFamily: "monospace", fontSize: "15px"}}>initial code</h1>
             <textarea
                 placeholder="Введите начальный код"
                 value={initialCode}
                 onChange={(e) => handleChange("initialCode", e.target.value)}
                 rows={6}
+                style={{ fontFamily: "monospace" }}
+            />
+            <h1 style={{ fontFamily: "monospace", fontSize: "15px"}}>input data</h1>
+            <textarea
+                placeholder="Введите входные данные"
+                value={inputCode}
+                onChange={(e) => handleChange("inputCode", e.target.value)}
+                rows={3}
+                style={{ fontFamily: "monospace" }}
+            />
+            <h1 style={{ fontFamily: "monospace", fontSize: "15px"}}>output data</h1>
+            <textarea
+                placeholder="Введите выходные данные"
+                value={outputCode}
+                onChange={(e) => handleChange("outputCode", e.target.value)}
+                rows={3}
                 style={{ fontFamily: "monospace" }}
             />
         </div>

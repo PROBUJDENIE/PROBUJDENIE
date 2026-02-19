9891edff455744c958051abcf1fd954e6a70dcec
Vitalia_Prokhorova <username@yandex.ru>
2026-02-19 21:09:25 +0300

    refactor: изменить раздел лекция

--- src/main/frontend/src/lecture/Lecture.jsx
+++ src/main/frontend/src/lecture/Lecture.jsx
@@ -3,19 +3,25 @@ import Header from "@/main-page/compoents/content/hero/header/Header.jsx";
 import "./lecture.css"
 import LectureBlocks from "@/lecture/LectureBlocks.jsx";
 import Bottom from "@/main-page/compoents/content/bottom/Bottom.jsx";
-import CommonBtn from "@/main-page/compoents/prototype/btn/CommonBtn.jsx";
 import Jump from "@/main-page/compoents/content/jump/Jump.jsx";
 import LectureManagerNavigation from "@/lecture/LectureManagerNavigation.jsx";
 import {useEffect, useState} from "react";
 import {lectureApi} from "@/api/lecture.api.js";
 import {useParams} from "react-router-dom";
-
+import menu from "./resourses/menu.svg"
+import emptySt from "./resourses/empty.svg";
+import next from "./resourses/next.svg"
 export default function Lecture() {
-    const { courseId } = useParams();
+    const {courseId} = useParams();
 
     const [isOpen, setIsOpen] = useState(false);
     const [activeLectureId, setActiveLectureId] = useState("null");
     const [lecture, setLecture] = useState(null);
+
+    const [activeChapter, setActiveChapter] = useState(null);
+    const [activeLectureNumber, setActiveLectureNumber] = useState(null);
+    const [totalLectures, setTotalLectures] = useState(null);
+
 
     useEffect(() => {
         if (activeLectureId === "null") {
@@ -36,19 +42,43 @@ export default function Lecture() {
     }, [activeLectureId]);
 
 
-
     return (
         <>
             <div className="lecture">
-                <Header />
+                <Header/>
                 <Jump></Jump>
-                <LectureManagerNavigation isOpen={isOpen} setOpen={() => setIsOpen(false)} activeLectureId={activeLectureId} setActiveLectureId={setActiveLectureId} courseId={courseId}></LectureManagerNavigation>
+                <LectureManagerNavigation isOpen={isOpen} setOpen={() => setIsOpen(false)}
+                                          activeLectureId={activeLectureId} setActiveLectureId={setActiveLectureId}
+                                          courseId={courseId} setActiveChapter={setActiveChapter}
+                                          setActiveLectureNumber={setActiveLectureNumber}
+                                          setTotalLectures={setTotalLectures} ></LectureManagerNavigation>
                 <Container>
                     <div className="lecture_content">
                         <div className="lecture_content-header">
-                            <CommonBtn width={55} height={55} borderColor={"black"} onClick={() => setIsOpen(true)}>=</CommonBtn>
+                            <div className="lecture_content-header_menu">
+                                <button className={"btnr"} onClick={() => setIsOpen(true)}><img src={menu} height={70} width={70}/></button>
+                                <div className="lecture_content-header_menu_box">
+                                    {activeLectureId !== "null" && ( <>
+                                            <h2>Глава {activeChapter}</h2>
+                                            <img src={next} alt="вправо" width={50} height={50}/>
+                                            <h2>
+                                                Лекция {activeLectureNumber}/{totalLectures}
+                                            </h2>
+                                        </>
+                                    )}
+                                </div>
+
+                            </div>
                         </div>
-                        {lecture?.content && (<LectureBlocks material={lecture.content}></LectureBlocks>)}
+                        {activeLectureId === "null" ? (
+                            <div className="lecture_empty-state">
+                                <img src={emptySt}/>
+                                <h2> Ты не выбрал лекцию(</h2>
+                                <h3> Выбери главу, затем лекцию и приступай к обучению!</h3>
+                            </div>
+                        ) : (
+                            lecture?.content && <LectureBlocks material={lecture.content}/>
+                        )}
                     </div>
                 </Container>
                 <Bottom></Bottom>
--- src/main/frontend/src/lecture/LectureManagerNavigation.jsx
+++ src/main/frontend/src/lecture/LectureManagerNavigation.jsx
@@ -3,7 +3,8 @@ import CommonBtn from "@/main-page/compoents/prototype/btn/CommonBtn.jsx";
 import {useEffect, useState} from "react";
 import {sectionApi} from "@/api/section.api.js";
 import {lectureApi} from "@/api/lecture.api.js";
-export default function LectureManagerNavigation({isOpen, setOpen, courseId , activeLectureId, setActiveLectureId}) {
+import menu from "@/lecture/resourses/menu.svg";
+export default function LectureManagerNavigation({isOpen, setOpen, courseId , activeLectureId, setActiveLectureId, setTotalLectures, setActiveLectureNumber, setActiveChapter}) {
 
     const [activeSection, setActiveSection] = useState("");
     const [sections, setSections] = useState([]);
@@ -43,7 +44,12 @@ export default function LectureManagerNavigation({isOpen, setOpen, courseId , ac
     const handleClickSection = (sectionId) => {
         setActiveSection(sectionId);
         setActiveLectureId("null");
-    }
+
+        setActiveChapter(null);
+        setActiveLectureNumber(null);
+        setTotalLectures(null);
+    };
+
 
     if (!isOpen) {
         return null;
@@ -53,21 +59,26 @@ export default function LectureManagerNavigation({isOpen, setOpen, courseId , ac
             <div className="login-box-overlay" onClick={setOpen}>
                 <div className="lectureManagerNavigation" onClick={e => e.stopPropagation()}>
                     <div className="lectureManagerNavigation_header">
-                        <CommonBtn width={55} height={55} borderColor={"black"} onClick={setOpen}>=</CommonBtn>
+                        <button  className={"btnr"} onClick={setOpen}><img src={menu} height={70} width={70} /></button>
                     </div>
                     <div className="lectureManagerNavigation_content">
                         {sections.map((section, index) => {
                             return (
                                 <>
                                     <div key={index} className="lectureManagerNavigation_content-section">
-                                        <CommonBtn width={270} height={40} borderColor={"black"} bgColor={(activeSection === section.id) ? "black" : "#fff"} onClick={() => handleClickSection(section.id)}>{section.title}</CommonBtn>
+                                        <CommonBtn size={17} width={270} height={60} borderColor={"#8A6CFF"} fontColor={(activeSection === section.id) ? "#ffffff" : "#333333"} bgColor={(activeSection === section.id) ? "#8A6CFF" : "#DFD8D3"} onClick={() => handleClickSection(section.id)}>{section.title}</CommonBtn>
                                     </div>
 
                                     {activeSection === section.id && (
 
-                                        lectures.map((lec) => (
-                                            <div className="lectureManagerNavigation_content-lecture">
-                                                <CommonBtn width={200} height={40} borderColor={"black"} bgColor={activeLectureId === lec.id ? "black" : "#fff"} onClick={() => setActiveLectureId(lec.id)}>{lec.title}</CommonBtn>
+                                        lectures.map((lec, lectureIndex) => (
+                                            <div key={lec.id} className="lectureManagerNavigation_content-lecture">
+                                                <CommonBtn size={17} width={200} height={60} borderColor={"#8A6CFF"} fontColor={(activeLectureId === lec.id) ? "#ffffff" : "#333333"} bgColor={activeLectureId === lec.id ? "#8A6CFF" : "#DFD8D3"} onClick={() => {
+                                                    setActiveLectureId(lec.id);
+                                                    setActiveChapter(index + 1);
+                                                    setActiveLectureNumber(lectureIndex + 1);
+                                                    setTotalLectures(lectures.length);
+                                                }}>{lec.title}</CommonBtn>
                                             </div>
                                         ))
                                     )}

889fcfe6abfbe6332f077710315abd0d1396a20e
Vitalia_Prokhorova <username@yandex.ru>
2026-02-21 21:37:22 +0300

    refactor: изменить фото курсов

--- src/main/frontend/src/main-page/compoents/content/bottom/Bottom.jsx
+++ src/main/frontend/src/main-page/compoents/content/bottom/Bottom.jsx
@@ -10,8 +10,8 @@ export default function Bottom() {
                 <div className="bottom-title">PROBUJDENIE</div>
                 <div className="bottom-desc">Образовательная платформа с игровым подходом к обучению</div>
                 <div className="bottom-btns">
-                    <CommonBtn leftIcon={<img src={teleg} width={50} height={50}/>} width={370} height={40} size={22} borderColor={"#8A6CFF"} fontColor={333333} onClick={() => window.open("https://web.telegram.org/a/#1133165164", "_blank") }>@V_BELIKOV_V</CommonBtn>
-                    <CommonBtn leftIcon={<img src={mail} width={50} height={50}/>} width={370} height={40} size={22} borderColor={"#8A6CFF"} fontColor={333333} onClick={() => window.open("https://web.telegram.org/a/#1133165164", "_blank") }>probujdenie@mail.ru</CommonBtn>
+                    <CommonBtn leftIcon={<img src={teleg} width={30} height={30}/>} width={370} height={40} size={22} borderColor={"#8A6CFF"} fontColor={333333} onClick={() => window.open("https://web.telegram.org/a/#1133165164", "_blank") }>@V_BELIKOV_V</CommonBtn>
+                    <CommonBtn leftIcon={<img src={mail} width={30} height={30}/>} width={370} height={40} size={22} borderColor={"#8A6CFF"} fontColor={333333} onClick={() => window.open("https://web.telegram.org/a/#1133165164", "_blank") }>probujdenie@mail.ru</CommonBtn>
                 </div>
                 <div className="bottom-rules">
                     <img className="bottom-rules_img" src={btImage} alt={""}></img>

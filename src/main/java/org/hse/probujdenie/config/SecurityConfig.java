9c3eb4edf473fe398a1c53f2e4cf31ea2682e5f5
Vladimir_Belikov <belikov_v2@mail.ru>
2026-04-08 16:13:41 +0300

    refactor: исправлен баг в авторизации

--- src/main/java/org/hse/probujdenie/config/SecurityConfig.java
+++ src/main/java/org/hse/probujdenie/config/SecurityConfig.java
@@ -32,6 +32,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/",
+                                "/user-profile",
+                                "/admin-profile",
                                 "/index.html",
                                 "/favicon.ico",
                                 "/assets/**",

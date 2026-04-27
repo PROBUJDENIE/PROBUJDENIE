a7fc676dd72520641987d811634f316c686b39a0
Vitalia_Prokhorova <vitalia12007@gmail.com>
2026-04-27 13:00:48 +0300

    refactor: исправить баги

--- src/main/frontend/src/api/hooks/useStudentCourses.js
+++ src/main/frontend/src/api/hooks/useStudentCourses.js
@@ -1,16 +1,20 @@
-import { useState, useEffect, useCallback } from 'react';
+import {useState, useCallback, useEffect} from 'react';
 import { studentApi } from '@/api/student.api.js';
+import {useAuth} from "@/autorisation/AuthContext.jsx";
 
 export const useStudentCourses = () => {
+    const { isAuthenticated } = useAuth();
     const [courses, setCourses] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     const loadCourses = useCallback(async () => {
+        if (!isAuthenticated) return;
         setLoading(true);
         setError(null);
         try {
             const response = await studentApi.getMyCourses();
+            console.log("getMyCourses response:", response);
             setCourses(response.data || []);
         } catch (err) {
             setError(err.message);
@@ -18,13 +22,23 @@ export const useStudentCourses = () => {
         } finally {
             setLoading(false);
         }
+    }, [isAuthenticated]);
+
+    const getPromoCodes = useCallback(async () => {
+        try {
+            const result = await studentApi.getPromoCodes();
+            return result.data || [];
+        } catch (err) {
+            console.error('Error:', err);
+            return [];
+        }
     }, []);
 
-    const buyCourse = useCallback(async (courseId) => {
+    const buyCourse = useCallback(async (courseId, promoCodeId = null) => {
         setLoading(true);
         setError(null);
         try {
-            const result = await studentApi.buyCourse(courseId);
+            const result = await studentApi.buyCourse(courseId, promoCodeId);
             if (result.success) {
                 await loadCourses();
                 return { success: true, data: result };
@@ -39,10 +53,9 @@ export const useStudentCourses = () => {
             setLoading(false);
         }
     }, [loadCourses]);
-
     useEffect(() => {
         loadCourses();
-    }, [loadCourses]);
+    }, [loadCourses, isAuthenticated]);
 
-    return {courses, loading, error, loadCourses, buyCourse};
+    return { courses, loading, error, loadCourses, buyCourse, getPromoCodes };
 };

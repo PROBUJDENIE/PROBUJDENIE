8f69da068981adc3885340343cb964ac6166438c
Vitalia_Prokhorova <vitalia12007@gmail.com>
2026-05-10 16:44:50 +0300

    fix: исправить баги

--- src/main/java/org/hse/probujdenie/util/CommonConstants.java
+++ src/main/java/org/hse/probujdenie/util/CommonConstants.java
@@ -7,4 +7,15 @@ public abstract class CommonConstants {
     public static class FileSaverConstants {
         public static final String BUCKET = "probujdenie";
     }
+
+    public static class MetricConstants {
+        public static final String USER_LOGIN_COUNT = "user.login.count";
+        public static final String CREATE_IMAGE_COUNT = "create.image.count";
+        public static final String START_CONTAINER_COUNT_ERROR = "start.container.count";
+        public static final String START_CONTAINER_COUNT_SUCCESS = "start.container.count";
+        public static final String USER_REGISTRATION_COUNT = "user.registration.count";
+        public static final String STUDENT_SUBMISSION_CHECK_DURATION = "student.submission.check.duration";
+
+        public static final String RESULT = "result";
+    }
 }
